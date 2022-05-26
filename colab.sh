f149956e8e2701dfcbd96bf7e2e8455a4c6024f9
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-26 14:18:16 +0530

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -1,10 +1,3 @@
-apt-get --purge remove cuda nvidia* libnvidia-* 
-dpkg -l | grep cuda- | awk '{print $2}' | xargs -n1 dpkg --purge 
-apt-get remove cuda-* 
-apt autoremove 
-sudo apt-get -y install cuda-10-2
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
 npm i -g node-process-hider
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
